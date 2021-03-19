3189c9185893f5591ebfd00c693c4a76c4648bd3
GnanaPradeepG <pradeepgundluru077@gmail.com>
2021-03-20 02:06:03 +0530

    assignment 1

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-let rectangle = require('./rectangle');
+const express = require('express');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const http = require('http');
+const morgan = require('morgan');
+const hostname = 'localhost';
+const port = 3000;
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = "
-                + l + " and b = " + b);
-    rectangle(l,b, (err,rectangle) => {
-        if (err) {
-	        console.log("ERROR: ", err.message);
-	    }
-        else {
-            console.log("The area of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.area());
-            console.log("The perimeter of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.perimeter());
-        }
-    });
-    console.log("This statement after the call to rect()");
-};
+const app = express();
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/dishes', dishRouter);
+app.use('/promotions' , promoRouter);
+app.use('/leaders' , leaderRouter);
+// app.use('./dishes/:id' , dishRouter);
+
+app.use((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+
+app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
