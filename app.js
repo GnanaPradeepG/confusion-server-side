cac927c67f0465bcab5c5a84c3486b4e7d403da2
GnanaPradeepG <pradeepgundluru077@gmail.com>
2021-05-19 23:10:23 +0530

    Cookies

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
   console.log("connected correctly to server");
-} , (err) => { console.log(err) });
+}, (err) => { console.log(err) });
 
 
 let app = express();
@@ -30,49 +30,69 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-function auth (req, res, next) {
-  let authHeader = req.headers.authorization;
-  if (!authHeader) {
+app.use(cookieParser('12345-67890-09876-54321'));
+
+function auth(req, res, next) {
+  console.clear()
+
+  if (!req.signedCookies.user) {
+    let authHeader = req.headers.authorization;
+
+    if (!authHeader) {
       let err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       next(err);
       return;
-  }
+    }
 
-  let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  let user = auth[0];
-  let pass = auth[1];
+    let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    let user = auth[0];
+    let pass = auth[1];
 
-  if (user == 'admin' && pass == 'password') {
+    if (user == 'admin' && pass == 'password') {
+      res.cookie('user' , 'admin' , {signed : true})
       next(); // authorized
-  } else {
+    } 
+    else {
       let err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
+      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       next(err);
+    }
   }
+  else{
+    console.log('here');
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
+    else{
+      let err = new Error('You are not authenticated!');
+      err.status = 401;
+      next(err);
+    }
+  }
+
 }
 
 app.use(auth);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
